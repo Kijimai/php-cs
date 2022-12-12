bd16e007a788237f7610857b460530fbb25c2a5f
John Deen Bucog <JDBucog@yahoo.com>
2022-12-12 12:41:48 -0800

    lesson 4

--- _starter_files/04_conditionals.php
+++ _starter_files/04_conditionals.php
@@ -33,3 +33,53 @@ if (condition) {
 
 
 /* -------- Switch Statements ------- */
+$age = 20;
+
+// if($age >= 18) {
+//   echo 'You are old enough to vote';
+// } else {
+//   echo 'Sorry you are not old enough to vote;';
+// }
+
+$timezoneId = "America/Los_Angeles";
+date_default_timezone_set($timezoneId);
+
+$t = date("H");
+// var_dump($t);
+// if($t < 12) {
+//   echo "Good Morning!";
+// } elseif ($t < 17) {
+//   echo "Good Afternoon";
+// } else {
+//   echo "Good evening";
+// }
+
+$posts = ["My First Post!"];
+
+// if (!empty($posts)) {
+//   echo $posts[0];
+// } else {
+//   echo "There are no posts right now.";
+// }
+
+// Ternary operator
+// echo !empty($posts) ? $posts[0] : 'There are no posts right now';
+
+// Coalescing Operator
+$firstPost = $posts[0] ?? null;
+// echo $firstPost;
+
+$favColor = "redss";
+
+switch($favColor) {
+  case 'red':
+    echo "Your favorite color is red";
+    break;
+  case "blue":
+    echo "Your favorite color is blue, dabadee.";
+    break;
+  default:
+    echo "We dont have any matching colors :(";
+}
+
+?>
